d481efb11a9f7c268acd5efc8dcb67fc491347ae
Pablo Sanchez Martinez <psanche2@ed.ac.uk>
2023-10-03 08:28:39 -0300

    New directories.

--- initialization.R
+++ initialization.R
@@ -15,6 +15,7 @@ dir.create("scripts/functions", showWarnings = F)
 dir.create("scripts/tests", showWarnings = F)
 dir.create("scripts/plotting", showWarnings = F)
 dir.create("scripts/data_processing", showWarnings = F)
+dir.create("scripts/analysis", showWarnings = F)
 
 dir.create("data_raw/", showWarnings = F)
 dir.create("data_raw/stem_water_potential", showWarnings = F)
@@ -26,6 +27,7 @@ dir.create("data_processed/stem_water_potential", showWarnings = F)
 dir.create("data_processed/stem_water_content", showWarnings = F)
 dir.create("data_processed/sapflow", showWarnings = F)
 dir.create("data_processed/met", showWarnings = F)
+dir.create("data_processed/soil_moisture", showWarnings = F)
 
 
 dir.create("outputs/", showWarnings = F)
@@ -35,7 +37,11 @@ dir.create("outputs/data_plots/stem_water_content", showWarnings = F)
 dir.create("outputs/data_plots/sapflow", showWarnings = F)
 dir.create("outputs/data_plots/increment", showWarnings = F)
 dir.create("outputs/data_plots/met", showWarnings = F)
+dir.create("outputs/data_plots/soil", showWarnings = F)
 
+dir.create("outputs/analysis", showWarnings = F)
+dir.create("outputs/analysis/stem_water_content_met", showWarnings = F)
+dir.create("outputs/analysis/sapflow_met", showWarnings = F)
 
 #### PACKAGES ------------------------------------------------------------------ ####
 
@@ -44,10 +50,19 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 library(lubridate)
-
+library(TrEvol)
+library(lme4)
+library(lmerTest)
+library(MuMIn)
+library(interactions)
+library(stringr)
 
 #### PARAMETERS ---------------------------------------------------------------- ####
 
 h <- 5
 w <- 5
+
+
+color_control <- "#1b9e77ff"
+color_tfe <- "#d95f02ff"
 
